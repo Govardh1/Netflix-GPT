15ca7c9c29dd4a9f9d754bb308ce260d631ca777
Govardh1 <govi.reddy420@gmail.com>
2024-07-03 14:34:31 +0530

    ....

--- src/utils/validate.jsx
+++ src/utils/validate.jsx
@@ -1,15 +1,15 @@
-export const checkValidData=(email,password,Name)=>{
+export const checkValidData=(email,password)=>{
     const isEmailValid=/^[A-Za-z0-9.%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,}$/.test(email)
     const isPasswordValid=/^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{6,})\S$/.test(password)
-    const isNamevalid=/(^[a-zA-Z][a-zA-Z\s]{0,20}[a-zA-Z]$)/.test(Name)
+    // const isNamevalid=/(^[a-zA-Z][a-zA-Z\s]{0,20}[a-zA-Z]$)/.test(Name)
     if (!isEmailValid) {
         return "Email-Id is not Valid"
     }
     if (!isPasswordValid) {
         return "Password is not valid"
     }
-    if(!isNamevalid){
-        return "Name is not Valid"
-    }
+    // if(!isNamevalid){
+    //     return "Name is not Valid"
+    // }
     return null;
 }
